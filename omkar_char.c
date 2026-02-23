92ab20f58e7f840013f774a3bf458ea7418f46f1
Omkar7637 <omkarkashidok@gmail.com>
2026-02-23 15:55:01 +0530

    kernel driver heartbeat : 23-02-2026 15:55:01

--- omkar_char.c
+++ omkar_char.c
@@ -1,1 +1,16 @@
-#include <linux/module.h> // Required for all kernal modules
+#include <linux/module.h>   // Required for all kernal modules
+#include <linux/kernel.h>   // printk()
+#include <linux/fs.h>       // file_operations structure 
+#include <linux/access.h>   // copy_to_user() and cpoy_from_user()
+
+// Name of the device taht will appera in /proc/devices
+#define DEVICE_NAME "Omkar_Device"
+
+// Class Name (Normally used when auto-creating /dev node via udev, not fully used here)
+#define CLASS_NAME "Omkar_Class"
+
+// Major number = ID given by kernal to your driver
+// Kernal uses this number to know which driver to call
+static int major;
+
+// 
